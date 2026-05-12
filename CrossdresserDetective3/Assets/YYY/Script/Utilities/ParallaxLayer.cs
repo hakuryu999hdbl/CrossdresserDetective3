9e6aa6a4bd3e8cd71aae1867dc5eefc670a42e6c
FT Girl <hakuryu999hdbl@gmail.com>
2026-05-12 23:08:14 +0900

    260512_OutSide

---
+++ CrossdresserDetective3/Assets/YYY/Script/Utilities/ParallaxLayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    Transform cameraTransform;
+    public float parallaxFactor = 0.5f;
+
+    private Vector3 startCameraPos;
+    private Vector3 startLayerPos;
+
+    void Start()
+    {
+        if (cameraTransform == null)
+            cameraTransform = Camera.main.transform;
+
+        startCameraPos = cameraTransform.position;
+        startLayerPos = transform.position;
+
+        StartCoroutine(UpdateParallaxAfterCamera());
+    }
+
+    IEnumerator UpdateParallaxAfterCamera()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+
+            float cameraDeltaX = cameraTransform.position.x - startCameraPos.x;
+
+            transform.position = new Vector3(
+                startLayerPos.x + cameraDeltaX * parallaxFactor,
+                startLayerPos.y,
+                startLayerPos.z
+            );
+        }
+    }
+}
